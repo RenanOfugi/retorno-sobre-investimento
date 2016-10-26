605091469e8378ceb6819f32b3ab57e783edfe34
RenanOfugi <renangyn2010@hotmail.com>
2016-10-26 14:54:45 -0300

    App v1.0 finalizado

--- app/src/main/java/com/example/renano/retornosobreinvestimento/CalculoROI.java
+++ app/src/main/java/com/example/renano/retornosobreinvestimento/CalculoROI.java
@@ -1,5 +1,14 @@
 package com.example.renano.retornosobreinvestimento;
 
 public class CalculoROI {
+
+    public float calculaROI(float ganho, float investimento){
+
+        float roi;
+
+        roi = ((ganho - investimento)/investimento)*100;
+
+        return roi;
+    }
 }
 
--- app/src/main/java/com/example/renano/retornosobreinvestimento/MainActivity.java
+++ app/src/main/java/com/example/renano/retornosobreinvestimento/MainActivity.java
@@ -1,15 +1,16 @@
 package com.example.renano.retornosobreinvestimento;
 
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText ganho;
     EditText investimento;
-    EditText juros;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +18,65 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void recebeValoresROI(View view){
+    public void atualizaValoresROI(View view) {
 
         ganho = (EditText) findViewById(R.id.ganho);
         investimento = (EditText) findViewById(R.id.investimento);
-        juros = (EditText) findViewById(R.id.juros);
+        TextView msg = (TextView) findViewById(R.id.roi);
+
+        if (naoVazio(ganho, investimento)) {
+
+            float roi = gerenciaDados(ganho, investimento);
+            assert msg != null;
+            msg.setText(roi + " %");
+        } else {
+            msg.setText("0");
+        }
+
+    }
+
+    private float gerenciaDados(EditText ganho, EditText investimento) {
+
+
+        float ganhofloat, investimentofloat;
+
+        ganhofloat = converterParaFloat(ganho);
+        investimentofloat = converterParaFloat(investimento);
+
+        CalculoROI calculoROI = new CalculoROI();
+
+        float roi;
+        roi = calculoROI.calculaROI(ganhofloat, investimentofloat);
+
+        return roi;
+    }
+
+    private boolean naoVazio(EditText ganho, EditText investimento) {
+
+        if (ganho.getText().toString().isEmpty() || investimento.getText().toString().isEmpty()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private float converterParaFloat(EditText dado) {
+
+        float dadofloat;
+        String dadostring;
+
+        dadostring = converterParaString(dado);
+        dadofloat = Float.parseFloat(dadostring);
+
+        return dadofloat;
+    }
+
+    private String converterParaString(EditText dado) {
+
+        String dadoString;
+
+        dadoString = dado.getText().toString();
+
+        return dadoString;
     }
 }
